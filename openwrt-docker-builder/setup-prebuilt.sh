9c4a614316e2ef3848fe7bab113227f97fbafb5f
Lanqing Huang <lqhuang@outlook.com>
2024-02-16 09:59:53 +0000

    refactor: Draft to use customized Dockerfile as base image

--- openwrt-docker-builder/setup-prebuilt.sh
+++ openwrt-docker-builder/setup-prebuilt.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-FILE_NAME="llvm-bpf-15.0.7.Linux-x86_64.tar.xz"
-DOWNLOAD_URL="https://downloads.openwrt.org/snapshots/targets/x86/64/${FILE_NAME}"
+PRE_INSTALL="wget curl ca-certificates xz-utils less git"
+BUILD_PRE_INSTALL="build-essential clang  make"
+MUSL_LIBC="argp-standalone musl-fts-dev musl-obstack-dev musl-libintl"
+MOLD="libncurses-dev libssl-dev rsync gawk unzip bzip2 python3 python3-distutils file libpam-dev"
+# "liblzma-dev libsnmp-dev"
+
+FILE_PATTERN="Linux-x86_64.tar.xz"
+BPF_TOOLCHAIN_DOWNLOAD_FILE="llvm-bpf-15.0.7.${FILE_PATTERN}"
+IMAGEBUILDER_DOWNLOAD_FILE="openwrt-imagebuilder-x86-64.${FILE_PATTERN}"
+SDK_DOWNLOAD_FILE="openwrt-sdk-x86-64_gcc-12.3.0_musl.${FILE_PATTERN}"
+TOOLCHAIN_DOWNLOAD_FILE="openwrt-toolchain-x86-64_gcc-12.3.0_musl.${FILE_PATTERN}"
+
+# FILE_HOST=mirrors.cloud.tencent.com/openwrt
+FILE_HOST=mirror.nju.edu.cn/openwrt
+VERSION_PATH="${VERSION_PATH:-snapshots}"
+DOWNLOAD_PATH="${VERSION_PATH}/targets/${TARGET}"
+TARGET="${TARGET:-x86/64}"
+
 DL_DIR="${PWD}/dl"
+
+curl "https://$FILE_HOST/$DOWNLOAD_PATH/sha256sums" -fs -o sha256sums
+echo $(grep "$FILE_PATTERN" sha256sums | cut -d "*" -f 2) | FILES=$(< /dev/stdin)
+for FILE in $FILES; do
+    echo "Downloading $FILE..."
+    curl -o "${DL_DIR}/${FILE}" -L "https://$FILE_HOST/$DOWNLOAD_PATH/$FILE"
+done
+
+WORKDIR="${PWD}/workdir"
+mkdir -p "${WORKDIR}"
+for FILE in $FILES; do
+    echo "Extracting $FILE..."
+    tar -xf "${DL_DIR}/${FILE}"  --strip=1 --no-same-owner -C ${WORKDIR}
+done
+
+##
 
 BUILD_HOST_DIR="${PWD}/build_dir/host"
 STAGING_HOST_DIR="${PWD}/staging_dir/host"
