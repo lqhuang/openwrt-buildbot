7249fe9fe3779692d9457bffacb63b7c0945c7b6
Lanqing Huang <lqhuang@outlook.com>
2024-03-04 16:53:48 +0800

    feat: Add basic files for mwan config

---
+++ profiles/x86_64-mwan/files/etc/uci-defaults/10-services-up.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# https://openwrt.org/docs/guide-user/network/zeroconfig/zeroconf#umdns
+# Even if the service comes with the base install, it must be enabled and started:
+service umdns enable
+service umdns start
+
+service irqbalance enable
+service irqbalance start
+
+# By default, NTP client is enabled and NTP server is disabled.
---
+++ profiles/x86_64-mwan/files/etc/uci-defaults/sing-box.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# # Enable sing-box
+# uci set sing-box.main.enabled=1
+# uci commit sing-box
+
+# exit 0
